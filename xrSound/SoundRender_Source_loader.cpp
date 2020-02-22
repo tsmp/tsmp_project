6a473c73dbddd2ee5c279b50e6b1ed6d6e329518
maks0 <maks0_00@inbox.ru>
2020-02-22 18:21:48 +0500

    -Не падаем при отсутствии звука

--- xrSound/SoundRender_Source_loader.cpp
+++ xrSound/SoundRender_Source_loader.cpp
@@ -127,19 +127,19 @@ void CSoundRender_Source::load(LPCSTR name)
 	fname				= N;
 
 	strconcat			(sizeof(fn),fn,N,".ogg");
-	if (!FS.exist("$level$",fn))	FS.update_path	(fn,"$game_sounds$",fn);
 
-#ifdef _EDITOR
-	if (!FS.exist(fn)){ 
+	if (!FS.exist("$level$",fn))	
+		FS.update_path	(fn,"$game_sounds$",fn);
+
+
+	if (!FS.exist(fn))
+	{ 
+		Msg("! Can't find sound '%s'", name);
 		FS.update_path	(fn,"$game_sounds$","$no_sound.ogg");
     }
-#endif
+
 	LoadWave			(fn);		//.R_ASSERT(wave);
 	SoundRender->cache.cat_create	(CAT, dwBytesTotal);
-
-//	if (dwTimeTotal<100)					{
-//		Msg	("! WARNING: Invalid wave length (must be at least 100ms), file: %s",fn);
-//	}
 }
 
 void CSoundRender_Source::unload()
