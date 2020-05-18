522beff56895284a3b290a21c59d4f95f8265893
maks0 <maks0_00@inbox.ru>
2020-05-18 15:25:47 +0500

    -Process event reject код подсмотрел в openxray

--- xrGame/xrServer_process_event_reject.cpp
+++ xrGame/xrServer_process_event_reject.cpp
@@ -1,8 +1,10 @@
 #include "stdafx.h"
 #include "xrserver.h"
 #include "xrserver_objects.h"
+#include "..\..\TSMP2_Build_Config.h"
 
-bool xrServer::Process_event_reject	(NET_Packet& P, const ClientID sender, const u32 time, const u16 id_parent, const u16 id_entity, bool send_message)
+bool xrServer::Process_event_reject	(NET_Packet& P, const ClientID sender, const u32 time, const u16 id_parent, 
+	const u16 id_entity, bool send_message)
 {
 	// Parse message
 	CSE_Abstract* e_parent = game->get_entity_from_eid(id_parent);
@@ -11,54 +13,73 @@ bool xrServer::Process_event_reject	(NET_Packet& P, const ClientID sender, const
 #ifdef DEBUG
 	Msg("sv reject. id_parent %s id_entity %s [%d]",ent_name_safe(id_parent).c_str(),ent_name_safe(id_entity).c_str(), Device.dwFrame);
 #endif
-	
-	if (!(e_parent && e_entity))
-	{
-		Msg("! error (e_parent && e_entity) , ignoring");
-		return 0;
-	}
- 
-	game->OnDetach(id_parent,id_entity);
 
-	if (0xffff == e_entity->ID_Parent) 
+	if (!e_entity)
 	{
-		Msg	("! ERROR: can't detach independant object. entity[%s:%d], parent[%s:%d], section[%s]", e_entity->name_replace(),id_entity,e_parent->name_replace(),id_parent, *e_entity->s_name);
+		Msg("! ERROR on rejecting: entity not found. parent_id = [%d], entity_id = [%d], frame = [%d].", id_parent,
+			id_entity, Device.dwFrame);
+
 		return false;
 	}
 
-	// Rebuild parentness
-
-	if (!(e_entity->ID_Parent == id_parent))
+	if (!e_parent)
 	{
-		Msg("! error e_entity->ID_Parent == id_parent, ignoring");
+		Msg("! ERROR on rejecting: parent not found. parent_id = [%d], entity_id = [%d], frame = [%d].", id_parent,
+			id_entity, Device.dwFrame);
+
 		return false;
 	}
 
-	if (!(e_entity->name_replace()))
-	{
-		Msg("! error e_entity->name_replace(), ignoring");
-		return false;
-	}
+#ifdef MP_LOGGING
+    Msg("--- SV: Process reject: parent[%d][%s], item[%d][%s]", id_parent, e_parent->name_replace(), id_entity,
+        e_entity->name());
+#endif // MP_LOGGING
+        
+    xr_vector<u16>& C = e_parent->children;
+    xr_vector<u16>::iterator c = std::find(C.begin(), C.end(), id_entity);
+    
+    if (c == C.end())
+    {
+        Msg("! WARNING: SV: can't find children [%s][%d] of parent [%s][%d]",e_entity->s_name.c_str(),id_entity,
+            e_parent->name_replace(),e_parent);
 
-	e_entity->ID_Parent	= 0xffff;
-	xr_vector<u16>& C = e_parent->children;
+        return false;
+    }
 
-	xr_vector<u16>::iterator c = std::find(C.begin(),C.end(),id_entity);
+    if (0xffff == e_entity->ID_Parent)
+    {
+        Msg("! ERROR: can't detach independant object. entity[%s][%d], parent[%s][%d], section[%s]",
+            e_entity->name_replace(), id_entity, e_parent->name_replace(), id_parent, e_entity->s_name.c_str());
 
-	if (!(C.end() != c, e_entity->name_replace(), e_parent->name_replace()))
-	{
-		Msg("! (C.end() != c, e_entity->name_replace(), e_parent->name_replace())");
-		return false;
-	}
+        return false;
+    }
 
-	C.erase(c);
+    // Rebuild parentness
 
-	// Signal to everyone (including sender)
-	if (send_message)
-	{
-		DWORD MODE		= net_flags(TRUE,TRUE, FALSE, TRUE);
-		SendBroadcast	(BroadcastCID,P,MODE);
-	}
-	
-	return true;
+    if (e_entity->ID_Parent != id_parent)
+    {
+        Msg("! ERROR: e_entity->ID_Parent = [%d]  parent = [%d][%s]  entity_id = [%d]  frame = [%d]",
+            e_entity->ID_Parent, id_parent, e_parent->name_replace(), id_entity, Device.dwFrame);
+        
+        CSE_Abstract* e_parent_from_entity = game->get_entity_from_eid(e_entity->ID_Parent);
+
+        if (!e_parent_from_entity)
+            Msg("! ERROR: cant find parent from entity");
+        else
+            Msg("! ERROR: parent from entity is [%s][%d]",e_parent_from_entity->name_replace(), e_entity->ID_Parent);
+    }
+
+    game->OnDetach(id_parent, id_entity);
+
+    e_entity->ID_Parent = 0xffff;
+    C.erase(c);
+
+    // Signal to everyone (including sender)
+    if (send_message)
+    {
+        DWORD MODE = net_flags(TRUE, TRUE, FALSE, TRUE);
+        SendBroadcast(BroadcastCID, P, MODE);
+    }
+
+    return true;
 }
